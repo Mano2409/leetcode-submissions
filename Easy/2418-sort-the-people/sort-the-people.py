e48967e1468e90e442ae6d0d080dbdcb5164a940
Mano Kumar S <saibagavath.mano@gmail.com>
2026-06-24 17:25:37 +0000

    Time: 8 ms (27.36%) | Memory: 19.5 MB (97.52%) - LeetSync

---
+++ Easy/2418-sort-the-people/sort-the-people.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        people = []
+
+        for i in range(len(names)):
+            people.append([heights[i], names[i]])
+
+        people.sort(reverse=True)
+
+        res = [name for height, name in people]
+
+        return res
